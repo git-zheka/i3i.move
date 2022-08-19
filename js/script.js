7bdc09c9d8dd415bec898b72cf54c8ebbc2702c5
Piotr Papa <zhekapapik31@gmail.com>
2022-08-19 20:59:35 +0300

    Hello

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ $('.carousel_arrow_left').click(function() {
     let url = $(this).find("a").attr("href");
     window.location.replace(url);
   });
+
+
+  $(".botton_send").click(function(){
+    let tel = $('input[type=tel]').val();
+    let name = $('input[type=text1]').val();
+    let message = 'Name: ' + name + ' Phone number: ' + tel;
+    let token = '5400413990:AAGCSpa1oBwWQEMu_vrG348fZCel2gSkCF8';
+    let chat_id = 5135005850;
+
+  $.ajax({
+      url:'https://api.telegram.org/bot'+token+'/sendMessage',
+      method:'POST',
+      data:{chat_id:chat_id,text:message},
+      success:function(){
+        alert('Дякуєм за звернення, наш менеджер незабаром вам затедефонує!');
+      }
+    });
+  });
+
