758d213b0d64284bbe2c46035e5428917aa38d80
Piotr Papa <zhekapapik31@gmail.com>
2022-08-19 14:37:47 +0300

    All

---
+++ js/doc.js
@@ -0,0 +1,9 @@
+$("#home_doc_download").click(function () {
+    window.location.href 
+        = "docs/home_doc.pdf";
+});
+
+$("#commercial_doc_download").click(function () {
+  window.location.href 
+      = "docs/comm_doc.pdf";
+});
